87eb2ca269d2d65467391fe23a238eae4fef426d
Roberto Rodrigues <me@rrodrigues.net>
2012-06-26 18:29:08 -0500

    amendment suggested by Alvim

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    # (r'^grappelli/', include('grappelli.urls')),
+    (r'^grappelli/', include('grappelli.urls')),
 )
 
 # Configuracao para o Grappelli
